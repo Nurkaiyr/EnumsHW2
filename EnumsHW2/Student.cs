c3b91a7916ec4a05a92052e3b6fe67f337f66fb5
Nurkaiyr <yedige21@gmail.com>
2017-12-27 02:14:54 +0600

    Второе задание

---
+++ EnumsHW2/Student.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnumsHW2
+{
+    public class Student
+    {
+        public string Name { get; private set; }
+        public string SurName { get; private set; }
+        public Gender Sex { get; private set; }
+
+        public string Group { get; private set; }
+        public double AverageScore { get; private set; }
+        public Study Study { get; private set; }
+
+        public double Salary { get; set; }
+
+        public Student(string name, string surName,
+            Gender sex, string group, double averageScore, Study study, double salary)
+        {
+            Name = name;
+            SurName = surName;
+            Sex = sex;
+
+            Group = group;
+            AverageScore = averageScore;
+            Study = study;
+
+            Salary = salary;
+        }
+
+        public void ShowInfo()
+        {
+            Console.WriteLine($"------------------\nФИО: {SurName} {Name} \n" +
+                $"Пол: {Sex}\t|Зарплата: {Salary} тг\n" +
+                $"Группа: {Group}\n" +
+                $"Средний балл: _{AverageScore}_\nВид обучения: {Study}\n");
+        }
+    }
+}
---
+++ EnumsHW2/Univercity.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnumsHW2
+{
+    public class Univercity
+    {
+        public List<Student> students;
+        public List<Student> hostel;
+
+        public Univercity()
+        {
+            students = new List<Student>();
+            hostel = new List<Student>();
+
+            Student[] newStudents =
+            {
+                new Student("Едиге", "Нуркайыр", Gender.Male, "ВТиПО", 5.0, Study.Очное, 100000),
+                new Student("Дуйсембаев", "Райымбек",  Gender.Male, "МЧС", 4.5, Study.Заочное, 70000),
+                new Student("Нургамитов", "Акжол", Gender.Male, "Теплоэнергетика", 4.0, Study.Очное, 650000),
+            };
+
+            foreach (var student in newStudents)
+                students.Add(student);
+        }
+
+        public void HostelForming()
+        {
+            double minimalSalary = 40000;
+
+            foreach (var student in students)
+                if (student.Salary < minimalSalary) hostel.Add(student);
+
+            students.Sort(ScoreComparer);
+
+            foreach (var student in students)
+                if (student.Salary >= minimalSalary) hostel.Add(student);
+
+            int index = 1;
+            foreach (var student in hostel)
+            {
+                Console.Write(index + ")"); index++;
+                student.ShowInfo();
+            }
+        }
+
+        private int ScoreComparer(Student student1, Student student2)
+        {
+            if (student1.AverageScore > student2.AverageScore) return -1;
+            else if (student1.AverageScore < student2.AverageScore) return 1;
+
+            return 0;
+        }
+    }
+}
